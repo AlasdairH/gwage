a5a4cb477c10af2c21471eabba298580a9048e71
unknown <alasdairhitchen@gmail.com>
2021-05-03 16:05:56 +0100

    Added GLFW and GLAD

--- core/src/main.cpp
+++ core/src/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
 int main()
 {
     std::cout << ("Hello World") << std::endl;
+    if (false == glfwInit())
+    {
+        std::cout << ("glfw failed to initialise") << std::endl;
+    }
+    else
+    {
+        std::cout << ("glfw initialised") << std::endl;
+    }
+
+    glfwTerminate();
 }
